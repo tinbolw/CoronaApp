4761edbe6468b76d693a46c02b424459e0383e51
tinbolw <tinbolw@gmail.com>
2020-06-06 14:28:23 -0700

    added code that gets info from the website

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("commit")
+import requests
+
+response = requests.get("https://www.worldometers.info/coronavirus/")
+status = response.status_code
+content = response.text
+if response:
+    print(status)
+    print("Success")
+    print(content)
+else:
+    print(status)
+    print("Error: Not found")
+
